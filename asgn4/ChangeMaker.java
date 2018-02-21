e5c41f84fa19d23b67d415b91fbf6f770769b787
Max Blau <max.blau@gmail.com>
2018-02-21 12:00:32 -0800

    Finished, need to test with Tester

--- asgn4/ChangeMaker.java
+++ asgn4/ChangeMaker.java
@@ -23,48 +23,125 @@ public class ChangeMaker {
          System.out.println("\nEnter a positive amount to be changed (enter 0 to quit): ");
          n = in.nextInt();
          if(n <= 0)break;
-         /*c = change_DP(n,d);
+         c = change_DP(n,d);
          System.out.println("\nDP algorithm results");
          System.out.println("Amount: " + n);
          System.out.print("Optimal distribution: ");
          int sum = 0;
          int count = 0;
-         for(int i = 0; i < k-1; i++){
-            sum += c[i]*d[i];
-            count += c[i];
-            if(c[i] > 0){
-               System.out.printf("%d*%dc",c[i].d[i]);
-            }
-            if(sum < n){
-               System.out.print(" + ");
-            }
-         }
-         System.out.printf("\nOptimal coin count: %d",count);
-*/
-         c = change_greedy(n,d);
-         System.out.println("\nGreedy algorithm results");
-         System.out.println("Amount: " + n);
-         System.out.print("Optimal distribution: ");
-         int sum = 0;
-         int count = 0;
-         for(int i = 0; i < k-1; i++){
+         for(int i = 0; i < k; i++){
             sum += c[i]*d[i];
             count += c[i];
             if(c[i] > 0){
                System.out.printf("%d*%dc",c[i],d[i]);
-            }
-            if(sum < n){
-               System.out.print(" + ");
+               if(sum < n){
+                  System.out.print(" + ");
+               }
             }
          }
-         System.out.printf("\nOptimal coin count: %d",count);
+         System.out.printf("\nOptimal coin count: %d\n",count);
+
+         c = change_greedy(n,d);
+         System.out.println("\nGreedy algorithm results");
+         System.out.println("Amount: " + n);
+         System.out.print("Optimal distribution: ");
+         sum = 0;
+         count = 0;
+         for(int i = 0; i < k; i++){
+            sum += c[i]*d[i];
+            count += c[i];
+            if(c[i] > 0){
+               System.out.printf("%d*%dc",c[i],d[i]);
+               if(sum < n){
+                  System.out.print(" + ");
+               }
+            }
+         }
+         System.out.printf("\nOptimal coin count: %d\n",count);
       }
       System.out.println("Thanks for playing, Good Bye.");
    }
 
    public static int[] change_DP(int n, int[] d){
-      return null;
-   }  
+      int[] c = new int[n+1];
+      int[] a = new int[n+1];
+      int[] res = new int[d.length];
+      //change_DP_Rec(0, d, c, a);
+
+      for(int j = 0; j < n+1; j++) {
+         if(j == 0) {
+            c[j] = 0;
+         }
+         else {
+            int min = Integer.MAX_VALUE;
+            int m = 0;
+            for(int i = 0; i < d.length; i++) {
+               if(j-d[i] < 0) continue;
+               if(min > c[j-d[i]]) {
+                  min = c[j-d[i]];
+                  m = i;
+               }
+            }
+            a[j] = m;
+            c[j] = 1 + min;
+         }        
+      }
+
+      int i = n;
+      while(i > 0) {
+         res[a[i]] += 1;
+         i = i - d[a[i]];
+      }
+
+      return res;
+   }
+
+   /*private static int change_DP_Rec(int j, int[] d, int[] c, int[] a) {
+      if(j < 0) {
+         return Integer.MAX_VALUE;
+      }
+      else if(j == 0) {
+         return 0;
+      }
+      
+      int min = Integer.MAX_VALUE;
+      int m = 0;
+      int temp;
+      for(int i = 0; i < d.length; i++) {
+         temp = change_DP_Rec(j-d[i], d, c, a) 
+         if(min > temp) {
+            min = temp;
+            m = i;
+         }
+      }
+      a[j] = m;
+      c[j] = 1 + min;
+      return c[j];
+   }
+
+   private static void change_DP_Rec(int j, int[] d, int[] c, int[] a) {
+      if(j < 0) {
+         //c[j] = Integer.MAX_VALUE;
+         return;
+      }
+      else if(j == 0) {
+         c[j] = 0;
+      }
+      else {
+         int min = Integer.MAX_VALUE;
+         int m = 0;
+         for(int i = 0; i < d.length; i++) {
+            if(j-d[i] < 0) continue;
+            if(min > c[j-d[i]]) {
+               min = c[j-d[i]];
+               m = i;
+            }
+         }
+         a[j] = m;
+         c[j] = 1 + min;
+      }
+      change_DP_Rec(j+1, d, c, a);
+   }*/
 
    public static int[] change_greedy(int n, int[] d){
       int value = n;
---
+++ asgn4/Tester.java
@@ -0,0 +1,42 @@
+/*
+ * CSC349
+ * Max Blau(mbblau) Jacob Territo(jterrito)
+ * Assignment 4: Change Making Proble
+ *
+ * 2/20/18
+ */
+import java.util.*;
+import java.io.*;
+
+public class Tester {
+   public static void main(String[] args) {
+      int[] us_denom = {100, 50, 25, 10, 5, 1};
+      int[] sov_denom = {100, 50, 20, 15, 10, 5, 3, 2, 1};
+      int[] two_denom = {64, 32, 16, 8, 4, 2, 1};
+      int[] nick_denom = {100, 50, 25, 10, 1};
+      int[] rand_denom = {66, 35, 27, 18, 10, 1};
+      int[][] testing_array = {us_denom, sov_denom, two_denom, nick_denom, rand_denom};
+      int[] count = {0, 0, 0, 0, 0};
+      int[] c;
+      for(int i = 1; i <= 200; i++) {
+         for(int j = 0; j < 5; j++) {
+            c = change_DP(i, testing_array[j]);
+            int k = c.length-1;
+            int dp = 0;
+            int greedy = 0;
+            while(k >= 0) {
+               dp += c[k--];
+            }
+            c = change_greedy(i, testing_array[j]);
+            k = c.length-1;
+            while(k >= 0) {
+               greedy += c[k--];
+            }
+            if(greedy == dp) {
+               count[j] += 1;
+            }
+         }
+      }
+   }
+}
+
